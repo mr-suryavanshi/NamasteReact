9146b29147799b9446dd566d75b8792aed535d83
Sachin Suryavanshi <sachin1081999@gmail.com>
2025-05-18 08:05:22 +0530

    initial push

--- App.js
+++ App.js
@@ -1,3 +1,32 @@
-const heading = React.createElement('h1',{id: 'heading'},"Hello React World what's up")
-        const root = ReactDOM.createRoot(document.getElementById('root'))
-        root.render(heading)
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-contanier">
+        <img
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+          className="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
